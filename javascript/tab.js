fc88dfa44d8ec2146e43cfb8f39f1225d6eceebc
acwell94 <89783182+acwell94@users.noreply.github.com>
2021-12-26 16:00:45 +0900

    Javascript
    Array, Object

--- javascript/tab.js
+++ javascript/tab.js
@@ -4,12 +4,24 @@
 // 1. 저 버튼0이 주황색으로 하이라이트 되어야 함
 // 2. 내용 0번째가 보여야함
 
-for (let i = 0; i < $('.tab-button').length; i++){
-$('.tab-button').eq(i).click(function(){
+// for (let i = 0; i < $('.tab-button').length; i++){
+// $('.tab-button').eq(i).click(function(){
+//     탭열기(i)
+// })
+// };
+
+    // 셀렉터.dataset.id
+
+$('.list').click(function(e){
+    // if(e.target == document.querySelectorAll('.tab-button')[0]){
+    //     탭열기(0)
+    // }
+    탭열기(e.target.dataset.id)
+})
+
+function 탭열기(숫자){
     $('.tab-button').removeClass('active');
     $('.tab-content').removeClass('show');
-    $('.tab-button').eq(i).addClass('active');
-    $('.tab-content').eq(i).addClass('show');
-})
-};
-
+    $('.tab-button').eq(숫자).addClass('active');
+    $('.tab-content').eq(숫자).addClass('show');
+}
